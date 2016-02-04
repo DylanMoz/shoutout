c3a0bd3604060238574457f49e6bc82f5cfe0c8f
Dylan Mozlowski <dylan.mozlowski@gmail.com>
2016-02-03 21:27:15 -0800

    Init Project

---
+++ controllers/api.js
@@ -0,0 +1,292 @@
+/**
+ * Split into declaration and initialization for better startup performance.
+ */
+var validator;
+var cheerio;
+var graph;
+var LastFmNode;
+var tumblr;
+var foursquare;
+var Github;
+var Twit;
+var stripe;
+var twilio;
+var Linkedin;
+var BitGo;
+var clockwork;
+var paypal;
+var lob;
+var ig;
+var Y;
+var request;
+
+var _ = require('lodash');
+var async = require('async');
+var querystring = require('querystring');
+
+/**
+ * GET /api
+ * List of API examples.
+ */
+exports.getApi = function(req, res) {
+  res.render('api/index', {
+    title: 'API Examples'
+  });
+};
+
+/**
+ * GET /api/facebook
+ * Facebook API example.
+ */
+exports.getFacebook = function(req, res, next) {
+  graph = require('fbgraph');
+
+  var token = _.find(req.user.tokens, { kind: 'facebook' });
+  graph.setAccessToken(token.accessToken);
+  async.parallel({
+    getMe: function(done) {
+      graph.get(req.user.facebook + "?fields=id,name,email,first_name,last_name,gender,link,locale,timezone", function(err, me) {
+        done(err, me);
+      });
+    },
+    getMyFriends: function(done) {
+      graph.get(req.user.facebook + '/friends', function(err, friends) {
+        done(err, friends.data);
+      });
+    }
+  },
+  function(err, results) {
+    if (err) {
+      return next(err);
+    }
+    res.render('api/facebook', {
+      title: 'Facebook API',
+      me: results.getMe,
+      friends: results.getMyFriends
+    });
+  });
+};
+
+/**
+ * GET /api/scraping
+ * Web scraping example using Cheerio library.
+ */
+exports.getScraping = function(req, res, next) {
+  cheerio = require('cheerio');
+  request = require('request');
+
+  request.get('https://news.ycombinator.com/', function(err, request, body) {
+    if (err) {
+      return next(err);
+    }
+    var $ = cheerio.load(body);
+    var links = [];
+    $('.title a[href^="http"], a[href^="https"]').each(function() {
+      links.push($(this));
+    });
+    res.render('api/scraping', {
+      title: 'Web Scraping',
+      links: links
+    });
+  });
+};
+
+/**
+ * GET /api/github
+ * GitHub API Example.
+ */
+exports.getGithub = function(req, res, next) {
+  Github = require('github-api');
+
+  var token = _.find(req.user.tokens, { kind: 'github' });
+  var github = new Github({ token: token.accessToken });
+  var repo = github.getRepo('sahat', 'requirejs-library');
+  repo.show(function(err, repo) {
+    if (err) {
+      return next(err);
+    }
+    res.render('api/github', {
+      title: 'GitHub API',
+      repo: repo
+    });
+  });
+
+};
+
+/**
+ * GET /api/aviary
+ * Aviary image processing example.
+ */
+exports.getAviary = function(req, res) {
+  res.render('api/aviary', {
+    title: 'Aviary API'
+  });
+};
+
+/**
+ * GET /api/nyt
+ * New York Times API example.
+ */
+exports.getNewYorkTimes = function(req, res, next) {
+  request = require('request');
+
+  var query = querystring.stringify({
+    'api-key': process.env.NYT_KEY,
+    'list-name': 'young-adult'
+  });
+  var url = 'http://api.nytimes.com/svc/books/v2/lists?' + query;
+
+  request.get(url, function(err, request, body) {
+    if (err) {
+      return next(err);
+    }
+    if (request.statusCode === 403) {
+      return next(Error('Missing or Invalid New York Times API Key'));
+    }
+    var bestsellers = JSON.parse(body);
+    res.render('api/nyt', {
+      title: 'New York Times API',
+      books: bestsellers.results
+    });
+  });
+};
+
+
+
+/**
+ * POST /api/twitter
+ * Post a tweet.
+ */
+exports.postTwitter = function(req, res, next) {
+  req.assert('tweet', 'Tweet cannot be empty.').notEmpty();
+
+  var errors = req.validationErrors();
+
+  if (errors) {
+    req.flash('errors', errors);
+    return res.redirect('/api/twitter');
+  }
+
+  var token = _.find(req.user.tokens, { kind: 'twitter' });
+  var T = new Twit({
+    consumer_key: process.env.TWITTER_KEY,
+    consumer_secret: process.env.TWITTER_SECRET,
+    access_token: token.accessToken,
+    access_token_secret: token.tokenSecret
+  });
+  T.post('statuses/update', { status: req.body.tweet }, function(err, data, response) {
+    if (err) {
+      return next(err);
+    }
+    req.flash('success', { msg: 'Tweet has been posted.'});
+    res.redirect('/api/twitter');
+  });
+};
+
+
+/**
+ * GET /api/stripe
+ * Stripe API example.
+ */
+exports.getStripe = function(req, res) {
+  stripe = require('stripe')(process.env.STRIPE_SKEY);
+
+  res.render('api/stripe', {
+    title: 'Stripe API',
+    publishableKey: process.env.STRIPE_PKEY
+  });
+};
+
+/**
+ * POST /api/stripe
+ * Make a payment.
+ */
+exports.postStripe = function(req, res, next) {
+  var stripeToken = req.body.stripeToken;
+  var stripeEmail = req.body.stripeEmail;
+  stripe.charges.create({
+    amount: 395,
+    currency: 'usd',
+    source: stripeToken,
+    description: stripeEmail
+  }, function(err, charge) {
+    if (err && err.type === 'StripeCardError') {
+      req.flash('errors', { msg: 'Your card has been declined.' });
+      res.redirect('/api/stripe');
+    }
+    req.flash('success', { msg: 'Your card has been charged successfully.' });
+    res.redirect('/api/stripe');
+  });
+};
+
+/**
+ * GET /api/twilio
+ * Twilio API example.
+ */
+exports.getTwilio = function(req, res) {
+  twilio = require('twilio')(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
+
+  res.render('api/twilio', {
+    title: 'Twilio API'
+  });
+};
+
+/**
+ * POST /api/twilio
+ * Send a text message using Twilio.
+ */
+exports.postTwilio = function(req, res, next) {
+  req.assert('number', 'Phone number is required.').notEmpty();
+  req.assert('message', 'Message cannot be blank.').notEmpty();
+
+  var errors = req.validationErrors();
+
+  if (errors) {
+    req.flash('errors', errors);
+    return res.redirect('/api/twilio');
+  }
+
+  var message = {
+    to: req.body.number,
+    from: '+13472235148',
+    body: req.body.message
+  };
+  twilio.sendMessage(message, function(err, responseData) {
+    if (err) {
+      return next(err.message);
+    }
+    req.flash('success', { msg: 'Text sent to ' + responseData.to + '.'});
+    res.redirect('/api/twilio');
+  });
+};
+
+/**
+ * GET /api/clockwork
+ * Clockwork SMS API example.
+ */
+exports.getClockwork = function(req, res) {
+  clockwork = require('clockwork')({ key: process.env.CLOCKWORK_KEY });
+
+  res.render('api/clockwork', {
+    title: 'Clockwork SMS API'
+  });
+};
+
+/**
+ * POST /api/clockwork
+ * Send a text message using Clockwork SMS
+ */
+exports.postClockwork = function(req, res, next) {
+  var message = {
+    To: req.body.telephone,
+    From: 'Hackathon',
+    Content: 'Hello from the Hackathon Starter'
+  };
+  clockwork.sendSms(message, function(err, responseData) {
+    if (err) {
+      return next(err.errDesc);
+    }
+    req.flash('success', { msg: 'Text sent to ' + responseData.responses[0].to });
+    res.redirect('/api/clockwork');
+  });
+};
